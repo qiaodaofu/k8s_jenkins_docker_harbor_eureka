24ff63d46733a74dedecf78d3f10c44ad703e63b
95-PC\95 <153880481@qq.com>
2019-04-06 18:28:38 +0800

    no message

---
+++ k8s_jenkins_demo/src/main/java/com/example/k8s_jenkins_demo/K8sJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8s_jenkins_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sJenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sJenkinsDemoApplication.class, args);
+    }
+
+}
---
+++ k8s_jenkins_demo/src/test/java/com/example/k8s_jenkins_demo/K8sJenkinsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.k8s_jenkins_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class K8sJenkinsDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
